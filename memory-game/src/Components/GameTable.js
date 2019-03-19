fb336eaea3da9ef4114236a3df74ac7bfadcc022
Philippe Recto <precto1285@gmal.com>
2019-03-18 20:15:52 -0700

    added component and app.js outline

---
+++ memory-game/src/Components/GameTable.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Row, Col } from 'reactstrap'
+const GameTable = props => {
+  return (
+    <div className="container text-center">
+      <Row>
+        <Col></Col>
+        <Col></Col>
+        <Col></Col>
+        <Col></Col>
+      </Row>
+      <Row>
+        <Col></Col>
+        <Col></Col>
+        <Col></Col>
+        <Col></Col>
+      </Row>
+      <Row>
+        <Col></Col>
+        <Col></Col>
+        <Col></Col>
+        <Col></Col>
+      </Row>
+    </div>
+  )
+}
+
+export default GameTable;
